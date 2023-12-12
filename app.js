e5d64c61acae09b67dd205d88356ad5fb26403c7
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2023-12-12 20:42:17 +0500

    completed

---
+++ app.js
@@ -0,0 +1,22 @@
+let speech =  new SpeechSynthesisUtterance();
+const btn = document.querySelector('button')
+let voices = []
+
+let voiceSelect = document.querySelector('select')
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name , i)))
+}
+
+voiceSelect.addEventListener('change' , ()=>{
+    speech.voice = voices[voiceSelect.value]
+})
+
+btn.addEventListener('click', ()=>{
+    // console.log('btn called')
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech);
+})
